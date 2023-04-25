586becd6a68b803b65d380f19038a2829659d727
4lourenco@gmail.com <marcos@cisne.i4.cp.utfpr.edu.br>
2023-04-25 15:26:53 -0300

    Arquivos cavity aula 18/04

--- cavity/plotalinha.py
+++ cavity/plotalinha.py
@@ -5,32 +5,48 @@
 #
 #############################################
 
-import os                  # Pastas e arquivos
+import os, sys          # Pastas e arquivos
 import numpy as np           # Vetores
 import matplotlib.pyplot as plt # Gráficos
 
 # Salvar as pastas dentro da lista 'pastas'
-pastas = os.listdir('testes')
+pasteste = str(sys.argv[1]) # Passaremos o
+# nome da pasta de testes via linha de comando
+pastas = os.listdir(pasteste)
+
+cor = 'C0'
 
 # Figura para o gráfico
 fig, ax1 = plt.subplots()
-ax2 = ax1.twinx().twiny()
+ax1.set_xlabel('$x[m]$')
+ax1.set_ylabel('$v[m/s]$')
+ax1.tick_params(axis='y', colors=cor)
+ax1.yaxis.label.set_color(cor)
+ax1.tick_params(axis='x', colors=cor)
+ax1.xaxis.label.set_color(cor)
+axt = ax1.twinx()
+axt.set_ylabel('$y[m]$')
+ax2 = axt.twiny()
+ax2.set_xlabel('$u[m/s]$')
 
-for pasta in pastas:
+tipos = ['-', '--', '-.', ':', 'dashedcondensed']
+
+for ip, pasta in enumerate(pastas):
     # U variando em y
-    arquy = r'testes/'+pasta+r'/uemy'
+    arquy = pasteste+f'/{pasta}/uemy'
     datuy = np.loadtxt(arquy)
     y, u = datuy.T # Colunas transpostas
 
     # V variando em x
-    arqvx = r'testes/'+pasta+r'/vemx'
+    arqvx = pasteste+f'/{pasta}/vemx'
     datvx = np.loadtxt(arqvx)
     x, v = datvx.T
 
-    ax1.plot(x, v, c='C0')
-    ax2.plot(u, y, c='C1')
+    ax1.plot(x, v, ls=tipos[ip], c=cor, label=pasta)
+    ax2.plot(u, y, ls=tipos[ip], c='0.3')
 
 
 fig.tight_layout()
+fig.legend()
 plt.show()
 
